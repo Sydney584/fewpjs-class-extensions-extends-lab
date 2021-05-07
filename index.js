acf5cb78c65fe085af97b587a09a89c9f79b7936
sydney <sydneychase5841@gmail.com>
2021-05-07 09:56:24 -0400

    triangle is valid count sides

--- index.js
+++ index.js
@@ -13,16 +13,37 @@ class Polygon {
 
     }
 }
+let polygon = new Polygon([5, 5, 5])
+console.log(polygon)
 
 class Triangle extends Polygon {
     constructor(sides) {
         super(sides);
 
     }
+    get isValid() {
+        if (!Array.isArray(this.sides)) return;
+        if (this.count !== 3) return;
+        let side1 = this.sides[0]
+        let side2 = this.sides[1]
+        let side3 = this.sides[2]
+        return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+    }
 }
+let triangle = new Triangle([5, 5, 5])
+let triangle2 = new Triangle([15, 10, 1])
+console.log(triangle)
+console.log(triangle2)
 
-class Square extends Triangle {
-    constructor() {
+class Square extends Polygon {
+    constructor(sides) {
+        super(sides)
 
+    }
+    get isValid() {
+
+    }
+    get area() {
+        
     }
 }
