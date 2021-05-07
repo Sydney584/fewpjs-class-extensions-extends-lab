d172da912c3b87c3351bede4b7a99ca1f88f3e0e
sydney <sydneychase5841@gmail.com>
2021-05-07 09:17:58 -0400

    complete Polygon test

--- index.js
+++ index.js
@@ -2,24 +2,27 @@
 class Polygon {
     constructor (sides) {
         this.sides = sides;
+        this.count = this.sides.length
         
     }
     get countSides() {
-        return this.sides
+        return this.sides.length
     }
     get perimeter() {
+        return this.sides.reduce((sum, side) => sum + side, 0);
 
     }
 }
 
 class Triangle extends Polygon {
-    constructor() {
+    constructor(sides) {
+        super(sides);
 
     }
 }
 
 class Square extends Triangle {
     constructor() {
-        
+
     }
 }
