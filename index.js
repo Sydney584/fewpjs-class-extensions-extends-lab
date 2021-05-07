e9deefa63c27d352a64a242a89901c5e7510adb0
sydney <sydneychase5841@gmail.com>
2021-05-07 10:10:24 -0400

    Assignment complete

--- index.js
+++ index.js
@@ -34,6 +34,10 @@ let triangle = new Triangle([5, 5, 5])
 let triangle2 = new Triangle([15, 10, 1])
 console.log(triangle)
 console.log(triangle2)
+triangle.isValid
+triangle2.isValid
+triangle.perimeter
+triangle2.perimeter
 
 class Square extends Polygon {
     constructor(sides) {
@@ -41,9 +45,22 @@ class Square extends Polygon {
 
     }
     get isValid() {
+        if (!Array.isArray(this.sides)) return;
+    if (this.count !== 4) return;
+    let side1 = this.sides[0]
+    let side2 = this.sides[1]
+    let side3 = this.sides[2]
+    let side4 = this.sides[3]
+    return ((side1 === side2) && (side1 === side3) && (side1 === side4))
 
     }
     get area() {
-        
+        if (!Array.isArray(this.sides)) return;
+    if (this.count !== 4) return;
+    return this.sides[0] * this.sides[0]
+
+
     }
 }
+let square = new Square([5, 5, 5, 5])
+console.log(square)
